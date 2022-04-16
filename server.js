054ef3879625f840a2ea966f43fdc4ef72f10dbb
Wellington <wellington3010@gmail.com>
2022-04-15 23:22:27 -0300

    Adding Procfile

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const appName = 'cineweb';
 
+const outputPath = `${__dirname}/dist/${appName}`;
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/cineweb'));
+app.use(express.static(outputPath));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/cineweb/'}),
-);
+app.get('/*', (req, res) => {
+    res.sendFile(`${outputPath}/index.html`);
+});
 
-app.listen(process.env.PORT || $PORT);
+app.listen(process.env.PORT);
