5e5d1b47812391b0ee7b604b42738fa5d9ffa9a7
Wellington <wellington3010@gmail.com>
2022-06-19 23:47:37 -0300

    Finishing cache implementation

--- src/app/store/movies.effects.ts
+++ src/app/store/movies.effects.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { EMPTY } from 'rxjs';
+import { EMPTY, Observable, of } from 'rxjs';
 import { map, mergeMap, catchError } from 'rxjs/operators';
+import { IMovie } from '../interfaces/IMovie';
 import { MoviesService } from '../services/movies.service';
  
 @Injectable()
 export class MovieEffects {
+  cache: Map<string, IMovie[]> = new Map();
  
   homeMovies$ = createEffect(() => this.actions$.pipe(
     ofType('[Home Movies] Movies'),
-    mergeMap(() => this.moviesService.getHomeMovies()
+    mergeMap(() => this.homeMoviesWithCache()
       .pipe(
         map(movies => {
           return ({ type: '[Home Movies] Movies Loaded with success', payload: movies });
@@ -21,7 +23,7 @@ export class MovieEffects {
 
   currentMovies$ = createEffect(() => this.actions$.pipe(
     ofType('[CurrentMovies Movies] Movies'),
-    mergeMap(() => this.moviesService.getCurrentMovies()
+    mergeMap(() => this.currentMoviesWithCache()
       .pipe(
         map(movies => {
           return ({ type: '[Current Movies] Movies Loaded with success', payload: movies });
@@ -33,7 +35,7 @@ export class MovieEffects {
 
   comingSoonMovies$ = createEffect(() => this.actions$.pipe(
     ofType('[ComingSoonMovies Movies] Movies'),
-    mergeMap(() => this.moviesService.getComingSoonMovies()
+    mergeMap(() => this.futureMoviesWithCache()
       .pipe(
         map(movies => {
           return ({ type: '[ComingSoonMovies Movies] Movies Loaded with success', payload: movies });
@@ -42,6 +44,42 @@ export class MovieEffects {
       ))
     )
   );
+
+  homeMoviesWithCache() : Observable<IMovie[]> {
+    if(this.cache.has("home-movies")) {
+      console.log("Well from cache");
+      return of(this.cache.get("home-movies")) as Observable<IMovie[]>;
+    }
+    else {
+      console.log("Well from api");
+      this.moviesService.getHomeMovies().subscribe(item  => this.cache.set("home-movies", item));
+      return this.moviesService.getHomeMovies();
+    }
+  }
+
+  currentMoviesWithCache() : Observable<IMovie[]> {
+    if(this.cache.has("current-movies")) {
+      console.log("current-movies from cache");
+      return of(this.cache.get("current-movies")) as Observable<IMovie[]>;
+    }
+    else {
+      console.log("current-movies from api");
+      this.moviesService.getCurrentMovies().subscribe(item  => this.cache.set("current-movies", item));
+      return this.moviesService.getCurrentMovies();
+    }
+  }
+
+  futureMoviesWithCache() : Observable<IMovie[]> {
+    if(this.cache.has("future-movies")) {
+      console.log("future-movies from cache");
+      return of(this.cache.get("future-movies")) as Observable<IMovie[]>;
+    }
+    else {
+      console.log("future-movies from api");
+      this.moviesService.getComingSoonMovies().subscribe(item  => this.cache.set("future-movies", item));
+      return this.moviesService.getComingSoonMovies();
+    }
+  }
  
   constructor(
     private actions$: Actions,
