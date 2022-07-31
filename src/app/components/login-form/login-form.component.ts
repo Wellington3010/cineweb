5ae3a9faf02414b770b877f4f7bff1e5311408d6
Wellington <wellington3010@gmail.com>
2022-07-31 20:56:25 -0300

    Adding form validations

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -26,17 +26,76 @@ export class LoginFormComponent implements OnInit {
   }
 
   submitForm() {
-    let nome = this.userForm.value['nome']
-    let email = this.userForm.value['email'];
+    let nome = this.userForm.value['nome'].toString().trimStart();
+    let email = this.userForm.value['email'].toString().trimStart();
     let senha = this.userForm.value['senha'];
 
-    if(this.page != "Cadastro")
+    let validacaoNome = this.validarNome(nome);
+    let validacaoEmail = this.validarEmail(email);
+    let validacaoSenha = this.validarSenha(senha);
+    
+
+    if(this.page != "Cadastro" && validacaoNome == "" && validacaoEmail == "" && validacaoSenha == "") {
       this.userManager.logarUsuario(email, senha);
+      this.userForm.reset();
+    }
 
-    if(this.page == "Cadastro")
-    this.userManager.cadastrarUsuario(nome, email, senha);
+    if(this.page == "Cadastro" && validacaoNome == "" && validacaoEmail == "" && validacaoSenha == "") {
+      this.userManager.cadastrarUsuario(nome, email, senha);
+      this.userForm.reset();
+    }
 
-    this.userForm.reset();
+    if(validacaoNome != "") {
+      alert(validacaoNome);
+    }
+
+    if(validacaoSenha != "") {
+      alert(validacaoSenha);
+    }
+
+    if(validacaoEmail != "") {
+      alert(validacaoEmail);
+    }
   }
 
+
+  validarNome(nome: string) : string {
+    if(nome.trim().length == 0) {
+      return "Campo nome deve ser preenchido";
+    }
+
+    if(nome.length > 40) {
+      return "Campo nome não pode conter mais que 40 caracteres";
+    }
+
+    return "";
+  }
+
+  validarEmail(email: string) : string {
+    if(email.trim().length == 0) {
+      return "Campo email deve ser preenchido";
+    }
+
+    if(email.split("@").length != 2) {
+      return "Email no formato inválido";
+    }
+
+    if(email.length > 70) {
+      return "Campo email não pode conter mais que 70 caracteres";
+    }
+
+    return "";
+  }
+
+  validarSenha(senha: string) : string {
+    if(senha.trim().length == 0) {
+      return "Campo senha deve ser preenchido";
+    }
+
+    if(senha.length > 30) {
+      return "Campo senha não pode conter mais que 30 caracteres";
+    }
+
+    return "";
+  }
 }
