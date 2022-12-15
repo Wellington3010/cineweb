367b453ec76e3665c97d68e7ce12d8e95a935b37
Wellington <wellington3010@gmail.com>
2022-12-15 19:31:25 -0300

    Corrigindo o path

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/cineweb/dist/${nomeApp}`));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/cineweb/dist/${nomeApp}/browser/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/browser/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
