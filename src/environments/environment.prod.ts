901c5d430175ebb7af796fa4201a5d07e64f36e2
Wellington <wellington3010@gmail.com>
2023-03-12 20:14:06 -0300

    Atualizando endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  moviesApiBaseUrl: "https://cineweb-movies-api-1722303389.sa-east-1.elb.amazonaws.com/movies",
-  usersApiBaseUrl:"https://cineweb-user-api-1019093983.sa-east-1.elb.amazonaws.com/users"
+  moviesApiBaseUrl: "http://cineweb-movies-api-1722303389.sa-east-1.elb.amazonaws.com/movies",
+  usersApiBaseUrl:"http://cineweb-user-api-1019093983.sa-east-1.elb.amazonaws.com/users"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  moviesApiBaseUrl: "https://cineweb-movies-api-1722303389.sa-east-1.elb.amazonaws.com/movies",
-  usersApiBaseUrl:"https://cineweb-user-api-1019093983.sa-east-1.elb.amazonaws.com/users"
+  moviesApiBaseUrl: "http://cineweb-movies-api-1722303389.sa-east-1.elb.amazonaws.com/movies",
+  usersApiBaseUrl:"http://cineweb-user-api-1019093983.sa-east-1.elb.amazonaws.com/users"
 };
 
 /*
