ac34764fd4df6f8bb671853c5d1edd95854199e7
Wellington <wellington3010@gmail.com>
2023-03-19 19:37:13 -0300

    Atualizando url dos enpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  moviesApiBaseUrl: "http://cineweb-movies-api-1722303389.sa-east-1.elb.amazonaws.com/movies",
-  usersApiBaseUrl:"http://cineweb-user-api-1019093983.sa-east-1.elb.amazonaws.com/users"
+  moviesApiBaseUrl: "http://cinew-loadb-tg84lzb5db3w-1547652695.sa-east-1.elb.amazonaws.com/movies",
+  usersApiBaseUrl:"http://cinew-loadb-17mrdhme2aewx-2003169029.sa-east-1.elb.amazonaws.com/users"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  moviesApiBaseUrl: "http://cineweb-movies-api-1722303389.sa-east-1.elb.amazonaws.com/movies",
-  usersApiBaseUrl:"http://cineweb-user-api-1019093983.sa-east-1.elb.amazonaws.com/users"
+  moviesApiBaseUrl: "http://cinew-loadb-tg84lzb5db3w-1547652695.sa-east-1.elb.amazonaws.com/movies",
+  usersApiBaseUrl:"http://cinew-loadb-17mrdhme2aewx-2003169029.sa-east-1.elb.amazonaws.com/users"
 };
 
 /*
